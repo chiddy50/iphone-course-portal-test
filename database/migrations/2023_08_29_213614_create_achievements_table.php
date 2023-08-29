93752f369186e19fbba891d021f6b06240113896
chiddy50 <codewelldev@gmail.com>
2023-08-29 23:01:06 +0100

    Added Achievement model, migration and seeder

---
+++ database/migrations/2023_08_29_213614_create_achievements_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Achievement name (e.g., "First Lesson Watched")
+            $table->enum('group', ['lesson', 'comment']);
+            $table->integer('required_count'); // Required count to unlock the achievement
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achievements');
+    }
+};
---
+++ database/seeders/AchievementSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Achievement;
+use Illuminate\Support\Facades\DB;
+
+class AchievementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('achievements')->truncate();
+
+        $achievements = [
+            [
+                'name' => 'first lesson watched',
+                'group' => 'lesson',
+                'required_count' => 1,
+            ],
+            [
+                'name' => '5 lessons watched',
+                'group' => 'lesson',
+                'required_count' => 5,
+            ],
+            [
+                'name' => '10 lessons watched',
+                'group' => 'lesson',
+                'required_count' => 10,
+            ],
+            [
+                'name' => '25 lessons watched',
+                'group' => 'lesson',
+                'required_count' => 25,
+            ],
+            [
+                'name' => '50 lessons watched',
+                'group' => 'lesson',
+                'required_count' => 50,
+            ],
+            [
+                'name' => 'first comment written',
+                'group' => 'comment',
+                'required_count' => 1,
+            ],
+            [
+                'name' => '3 comments written',
+                'group' => 'comment',
+                'required_count' => 3,
+            ],
+            [
+                'name' => '5 comments written',
+                'group' => 'comment',
+                'required_count' => 5,
+            ],
+            [
+                'name' => '10 comments written',
+                'group' => 'comment',
+                'required_count' => 10,
+            ],
+            [
+                'name' => '20 comments written',
+                'group' => 'comment',
+                'required_count' => 20,
+            ]
+        ];
+
+        foreach ($achievements as $achievement){
+            $achievement = Achievement::create($achievement);
+        }
+    }
+}
